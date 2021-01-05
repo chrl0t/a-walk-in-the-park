1d9bace86b12f9b776df2f43beabf034037ad760
leahjwoodhead <leahjwoodhead@gmail.com>
2021-01-05 09:29:53 +0000

    ad get request to firestore

---
+++ src/components/Loading.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './loading.css'
+
+const Loading = () => {
+    return (
+        <div class="walking">
+            <div class="head"></div>
+            <div class="body"></div>
+            <div class="firstLeg"></div>
+            <div class="secondLeg"></div>
+            <div class="shadow"></div>
+        </div>
+    );
+};
+
+export default Loading;
