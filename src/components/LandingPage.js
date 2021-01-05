2b356e78ee02dc848179f63c90e98973fa1de22e
chrl0t <charlotte_i@live.co.uk>
2021-01-05 16:02:12 +0000

    added styling to landing page and post ad

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,27 @@
 import React from "react";
+import logo from "../images/logo.png";
+import { LandingPageStyled } from "../styles";
+import { Link } from "@reach/router";
 
 const LandingPage = () => {
-  return <div className='landing'>Hi</div>;
+  return (
+    <LandingPageStyled className='landing'>
+      <img className='logo' src={logo} alt='logo' />
+      <p>
+        I'm baby truffaut iPhone tofu vegan butcher XOXO. Sustainable cardigan
+        hoodie tumblr pinterest fashion axe. 3 wolf moon pour-over meh pitchfork
+        try-hard ennui.
+      </p>
+      <Link to='/home'>
+        {" "}
+        <button className='login-button'>Login</button>
+      </Link>
+      <Link to='/home'>
+        {" "}
+        <button className='register-button'>I'm new here</button>
+      </Link>
+    </LandingPageStyled>
+  );
 };
 
 export default LandingPage;
--- src/components/PostAd.js
+++ src/components/PostAd.js
@@ -1,46 +1,53 @@
-import React, { useState } from 'react';
-import db from '../firebase'
-import firebase from 'firebase'
+import React, { useState } from "react";
+import db from "../firebase";
+import firebase from "firebase";
+import { AdForm } from "../styles";
 
 const PostAd = () => {
-    const [title, setTitle] = useState("")
-    const [body, setBody] = useState("")
-    const user = "grandpajoe"
-    const postcode =  new firebase.firestore.GeoPoint(53.7980924, -1.5337288)
-    const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-    
-    
-    const handleSubmit = (e) => {
-        e.preventDefault()
-      
-        db.settings({
-            timestampsInSnapshots: true
-        })
-        const adRef = db.collection("ads").add({
-            title: title,
-            description: body,
-            username: user,
-            "created at": timestamp,
-            postcode: postcode   
-        })
-        setTitle("")
-        setBody("")
-    }
-    console.log(Date().slice(4, 15))
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const user = "grandpajoe";
+  const postcode = new firebase.firestore.GeoPoint(53.7980924, -1.5337288);
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp();
 
-    return (
-        <div>
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <p>Title</p>
-                <input type="text" required onChange={(e) => setTitle(e.target.value)}/>
-                <p>Body</p>
-                <textarea type="text" required onChange={(e) => setBody(e.target.value)}/>
-                <input type="submit"/>
-            </form>
-            
-        </div>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    db.settings({
+      timestampsInSnapshots: true
+    });
+    const adRef = db.collection("ads").add({
+      title: title,
+      description: body,
+      username: user,
+      "created at": timestamp,
+      postcode: postcode
+    });
+    setTitle("");
+    setBody("");
+  };
+
+  return (
+    <AdForm>
+      <form className='ad-form' onSubmit={(e) => handleSubmit(e)}>
+        <h2>Ad Title</h2>
+        <input
+          type='text'
+          required
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <h2>Ad</h2>
+        <textarea
+          type='text'
+          required
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <br></br>
+        <br></br>
+        <input type='submit' className='submit-button' />
+      </form>
+    </AdForm>
+  );
 };
 
 export default PostAd;
-
