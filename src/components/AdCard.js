7f24209448df34a0ad7ae27292eb49f4f9406e46
chrl0t <charlotte_i@live.co.uk>
2021-01-06 16:57:08 +0000

    did some styling

--- src/components/AdCard.js
+++ src/components/AdCard.js
@@ -1,13 +1,12 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { AdCardStyled } from "../styles";
-import db from '../firebase';
+import db from "../firebase";
 import { Link } from "@reach/router";
 
 import { calculateDistance } from "../utils/calculateDistance";
-import Loading from './Loading';
+import Loading from "./Loading";
 import profilepic from "../images/profilepic.png";
-const postcodes = require('node-postcodes.io');
-
+const postcodes = require("node-postcodes.io");
 
 const AdCard = (props) => {
   const [loading, setLoading] = useState(true);
@@ -15,62 +14,64 @@ const AdCard = (props) => {
   const { ad, profile } = props;
 
   useEffect(() => {
-    getDistance(profile.postcode, ad.postcode)
-    .then((distance)=> {
+    getDistance(profile.postcode, ad.postcode).then((distance) => {
       setDistance(distance);
       setLoading(false);
-    })
-    
-}, [])
+    });
+  }, []);
 
-
-async function getGeolocation(postcode){
-  const result = await postcodes.lookup(postcode).then((res)=>{
-    return res.result;
-  })
-  return result;
-}
-
-// .replace(/\s/g,'')
-
-async function getDistance(profilePostcode, adPostcode){
-  if(adPostcode && profilePostcode){
-    let pp = await getGeolocation(profilePostcode);
-    let ap = await getGeolocation(adPostcode);
-    console.log(pp);
-    console.log(ap);
-
-    return calculateDistance(pp.latitude, pp.longitude, ap.latitude, ap.longitude);
+  async function getGeolocation(postcode) {
+    const result = await postcodes.lookup(postcode).then((res) => {
+      return res.result;
+    });
+    return result;
   }
-}
 
-  
+  // .replace(/\s/g,'')
+
+  async function getDistance(profilePostcode, adPostcode) {
+    if (adPostcode && profilePostcode) {
+      let pp = await getGeolocation(profilePostcode);
+      let ap = await getGeolocation(adPostcode);
+
+      return calculateDistance(
+        pp.latitude,
+        pp.longitude,
+        ap.latitude,
+        ap.longitude
+      );
+    }
+  }
+
   if (loading) {
+    return <Loading />;
+  } else {
+    // console.log(profile.postcode, 'profile-------------')
+    // console.log(ad.postcode, 'ad-------------------')
     return (
-        <Loading/>
-    )
-}  else {
-  // console.log(profile.postcode, 'profile-------------')
-  // console.log(ad.postcode, 'ad-------------------')
-  return (
-    <AdCardStyled>
-      <div className='ad_user'>
-        <Link className='username' to={`/user/${ad.username}`}>
-          <img src={profilepic} alt='user profile pic' />
-          <p>{ad.username}</p>{" "}
-        </Link>
-      </div>
-      <div className='ad_info'>
-        <h2>{ad.title}</h2>
-        <span>{ad.description}</span>
-        <br></br>
-        <button>Message</button>
-        <button>♡</button>
-        {(distance > 1 ? <button>{distance} miles from you</button>: <button>Less than a mile</button>)}
-      </div>
-    </AdCardStyled>
-  );
-}
+      <AdCardStyled>
+        <div className='ad_user'>
+          <Link className='username' to={`/user/${ad.username}`}>
+            <img src={profilepic} alt='user profile pic' />
+            <p>{ad.username}</p>{" "}
+          </Link>
+        </div>
+        <div className='ad_info'>
+          <h2>{ad.title}</h2>
+          <br></br>
+          <span>{ad.description}</span>
+          <br></br>
+          <button className='emoji'>✉️</button>
+          <button className='emoji'>❤️</button>
+          {distance > 1 ? (
+            <button className='distance'>{distance} miles away</button>
+          ) : (
+            <button className='distance'>Less than a mile away</button>
+          )}
+        </div>
+      </AdCardStyled>
+    );
+  }
 };
 
 export default AdCard;
--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,20 @@
-import firebase from 'firebase'
-import firebaseui from 'firebaseui';
+import firebase from "firebase";
+var firebaseui = require("firebaseui");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDXdozot24PgJ05qth7stWp6NdcrJqHo4Y",
-    authDomain: "a-walk-in-the-park-89cc0.firebaseapp.com",
-    projectId: "a-walk-in-the-park-89cc0",
-    storageBucket: "a-walk-in-the-park-89cc0.appspot.com",
-    messagingSenderId: "185033358747",
-    appId: "1:185033358747:web:903c7e5c778149412a353e",
-    measurementId: "G-8BKQV908L3"
-  };
+  apiKey: "AIzaSyDXdozot24PgJ05qth7stWp6NdcrJqHo4Y",
+  authDomain: "a-walk-in-the-park-89cc0.firebaseapp.com",
+  projectId: "a-walk-in-the-park-89cc0",
+  storageBucket: "a-walk-in-the-park-89cc0.appspot.com",
+  messagingSenderId: "185033358747",
+  appId: "1:185033358747:web:903c7e5c778149412a353e",
+  measurementId: "G-8BKQV908L3"
+};
 
-  
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-const db = firebase.firestore()
-const ui = new firebaseui.auth.AuthUI(firebase.auth())
+const db = firebase.firestore();
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-export default db
+export default db;
